520d0eefe026d2ea89f5df7146aac3af679c42bf
CaptainDeathead <unstableplazma@gmail.com>
2024-06-22 16:42:40 +0800

    Untested updater

--- updater.py
+++ updater.py
@@ -21,22 +21,22 @@ class Updater:
         self.download_and_install()
 
     def load_version(self) -> None:
-        print("Verifying `__version__.txt` exists...", end='', flush=True)
+        print("Verifying '__version__.txt' exists...", end='', flush=True)
 
         if not os.path.exists(f"{self.PATH}/__version__.txt"):
-            print("\rVerifying `__version__.txt` exists...  Failed!", flush=True)
+            print("\rVerifying '__version__.txt' exists...  Failed!", flush=True)
 
-            print("\nThe updater cannot find `__version__.txt`!\nPlease ensure it is present in the projects directory.")
+            print("\nThe updater cannot find '__version__.txt'!\nPlease ensure it is present in the projects directory.")
             exit()
         else:
-            print("\rVerifying `__version__.txt` exists...  Done!", flush=True)
+            print("\rVerifying '__version__.txt' exists...  Done!", flush=True)
 
-        print("Loading `__version__.txt`...", end='', flush=True)
+        print("Loading '__version__.txt'...", end='', flush=True)
 
         with open(f"{self.PATH}/__version__.txt", "r") as version_file:
             self.version_raw: str = version_file.read()
 
-        print("\rLoading `__version__.txt`...  Done!", flush=True)
+        print("\rLoading '__version__.txt'...  Done!", flush=True)
 
     def parse_version(self) -> None:
         print("Extracting version...", end='', flush=True)
@@ -66,6 +66,8 @@ class Updater:
             print("Please ensure you are connected to the internet!")
             exit()
 
+        print(f"\rRequesting the latest version at '{self.RAW_GITHUB_BASE_URL}/__version__.txt'...  Done!", flush=True)
+
         self.online_version_raw: str = version_request.text
         
         print("Extracting online version...", end='', flush=True)
@@ -76,8 +78,52 @@ class Updater:
         self.online_files: List[str] = [filename for filename in self.online_version_raw.splitlines()[1:]]
         print(f"\rDiscovering online files...  {len(self.online_files)} found!", flush=True)
 
+    def contrast_differences(self) -> None:
+        self.needs_update: bool = False
+
+        print("Comparing versions...", end='', flush=True)
+        if self.version == self.online_version:
+            print("\rComparing versions...  Same!", flush=True)
+
+            print("Comparing file counts...", end='', flush=True)
+            if len(self.installed_files) != len(self.online_files):
+                print("\rComparing file counts...  Different!", flush=True)
+                self.needs_update = True
+            else:
+                print("\rComparing file counts...  Same!", flush=True)
+        else:
+            print("\rComparing versions...  Different!", flush=True)
+            self.needs_update = True
+
     def get_latest_changes(self) -> None:
         self.parse_online_version()
+        self.contrast_differences()
+
+    def download_file(self, filename: str) -> None:
+        print(f"Downloading '{filename}'...", end='', flush=True)
+
+        try: file_request = requests.get(f"{self.RAW_GITHUB_BASE_URL}/{filename}")
+        except requests.RequestException:
+            print(f"\rDownloading '{filename}'...  Failed!\n", flush=True)
+            print(traceback)
+            print("Please ensure you are connected to the internet!")
+            exit()
+
+        if file_request.status_code >= 400:
+            print(f"\rDownloading '{filename}'...  ERROR: {file_request.status_code}!\n", flush=True)
+            print("Please ensure you are connected to the internet!")
+            exit()
+
+        print(f"Downloading '{filename}'...  Done!", flush=True)
+
+        print(f"Installing '{filename}'...", end='', flush=True)
+        with open(f"{self.PATH}/{filename}", "wb") as newfile:
+            newfile.write(file_request.content)
+        print(f"Installing '{filename}'...  Done!", flush=True)
+
+    def download_and_install(self) -> None:
+        for filename in self.online_files:
+            self.download_file(filename)
 
 def main() -> None:
     updater: Updater = Updater()
